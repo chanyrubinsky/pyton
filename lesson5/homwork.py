05d3e2fd9aac1cb4b9eb186fb3238657e7c9b138
huser <c0527601272@gmail.com>
2025-11-17 13:43:51 +0200

    create file in folder

--- lesson5/homwork.py
+++ lesson5/homwork.py
@@ -1,4 +1,24 @@
 import os
 
-os.mkdir('Z:\יד תשפו\רובינסקי חנה\piton\lesson5\newFolder')
 
+# os.mkdir('mewFolder')
+
+def deleteFolder():
+    try:
+        os.rmdir('mew folder')
+        print(f"התיקיה {'mew folder'} נמחקה בהצלחה.")
+    except OSError as e:
+        print(f"שגיאה במחיקת התיקיה {'mew folder'}: {e}")
+
+
+# deleteFolder()
+
+
+def create_file(link , f):
+    file = os.path.join(link, f)
+    os.makedirs(link, exist_ok=True)
+    with open(file, 'w') as f:
+        f.write("")
+
+
+create_file('Z:\יד תשפו\רובינסקי חנה\piton\pyton\lesson5','file.txt')
